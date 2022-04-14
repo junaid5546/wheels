09758a4a2787a8eaeab5d0b0dc54c580d6df3c52
Muhammad Gul <99083434+muhammadgulmi@users.noreply.github.com>
2022-04-14 23:39:33 +0400

    filters

--- src/app/filter/filter.module.ts
+++ src/app/filter/filter.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { IonicModule } from '@ionic/angular';
 
 import { FilterPageRoutingModule } from './filter-routing.module';
