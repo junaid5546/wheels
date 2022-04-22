fff0e2930de8464cf0d468f67a65e5e44adfb183
Muhammad Gul <99083434+muhammadgulmi@users.noreply.github.com>
2022-04-22 23:47:00 +0400

    tests for register

--- cypress/integration/initial-test.js
+++ cypress/integration/initial-test.js
@@ -1,9 +1,19 @@
 /// <reference types="cypress" />
 describe('web App testing', () =>{
     beforeEach(()=>{
-        cy.viewport('iphone-4')
+        cy.viewport('iphone-xr')
     })
     it('Landing Page works', ()=>{
         cy.visit('http://localhost:8100');
+        
+        cy.log('Loaded successfully');
+        cy.get('ion-select').click().children().should('contain','+968');
+        cy.contains('.alert-radio-label', '+968').click();
+        cy.contains('OK').click();
     })
+
+    it('Putting cell phone number', ()=>{
+       cy.get('ion-item').should('have.class','item-inner').children('ion-input').first().type(9);
+    })
+
 })
