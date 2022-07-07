350f6095e72e14ffce02f0e29f3183d242908182
Muhammad Gul <99083434+muhammadgulmi@users.noreply.github.com>
2022-07-07 12:43:48 +0400

    removed unnecessary things

--- src/app/Components/settings/settings-routing.module.ts
+++ src/app/Components/settings/settings-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SettingsComponent } from './settings.component';
-import { ContactUsComponent  } from '../contact-us/contact-us.component';
 
 const routes: Routes = [
   {
     path: '',
     component: SettingsComponent,
-    children:[
-        {
-            path:'contact-us',
-            component:ContactUsComponent
-        }
-    ]
+  
   }
 ];
 
