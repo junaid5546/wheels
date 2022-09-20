f09222f5e565146ed234ef9397299a9b664206a1
Muhammad Gul <99083434+muhammadgulmi@users.noreply.github.com>
2022-09-20 11:34:26 +0400

    merged with new code

--- src/app/Classes/Vehicle.ts
+++ src/app/Classes/Vehicle.ts
@@ -3,54 +3,36 @@
  * (Type docs)
  * @author Muhammad Junaid Gul <muhammad.gul.mi@outlook.com>
  */
+ import { _name } from '../Interface/Name';
 
-export class identity {
-  protected _id!: string
-  protected name!: _name
-}
-export class Make extends identity {
-  private clicked?: boolean
-  private completed?: boolean
-  private show?: boolean
-  private models: [Model]
-  constructor () {
-    super();
-  } 
-}
-
-export interface Engine {
-    _id:string;
-    name:_name;
-}
-
-export interface Bodies {
-    _id:string;
-    name:_name;
-    maxPrice: number;
-    minPrice: number;
-    image: string;
-    filtersId:[string];
-    doorCount:[DoorCount];
-    error:string;
-}
-
-export interface Trim {
-    _id:string;
-    name:_name;
-    bodies: [Bodies]
-    engineSize: [Engine]
-}
-export class FilterItem {
-    _id!:string;
-    name!:_name;
-}
-
-export class Make extends FilterItem {
-    clicked?:boolean = false;
-    completed?:boolean = false;
-    models:[Model];
-
-    constructor(){
-        super();
-    }
-}
+ export class identity {
+   protected _id!: string
+   protected name!: _name
+ }
+ export class Make extends identity {
+   private clicked?: boolean
+   private completed?: boolean
+   private show?: boolean
+   private models: [Model]
+   constructor () {
+     super();
+   } 
+ }
+ 
+  class Model extends Make  {
+     private trims: [Trim];
+ }
+ 
+ class Trim extends Model {
+   private bodies: [Bodies]
+   private engineSize: [identity]
+ }
+ 
+ class Bodies extends Trim {
+   maxPrice: number
+   minPrice: number
+   image: string
+   filtersId: [string]
+   doorCount: [identity]
+   error: string
+ }
